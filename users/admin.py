4307b8a1669a9c05af4dce4a8c3c7ac63e3e4100
kangraemin <ee4787@naver.com>
2020-03-08 01:15:40 +0900

    Make user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import (
+    models,
+)  # 해당 admin.py 와 같은 directory에 있는 모든 파일 ( apps.py .... )들을 불러오고 그 중에 models.py를 가져온다.
 
 # Register your models here.
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    """ Custom User Admin """
+
+    # see the difference is or not in admin pannel
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            (
+                "Custom Profile",
+                {
+                    "fields": (
+                        "avatar",
+                        "gender",
+                        "bio",
+                        "birthdate",
+                        "language",
+                        "currency",
+                        "superhost",
+                    )
+                },
+            ),
+        )
+    )
