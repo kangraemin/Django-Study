6654a225ca4f9393b441083d92d716868926f7e8
kangraemin <ee4787@naver.com>
2020-06-07 22:22:03 +0900

    Customize room admin ( add search field / list_display )

--- rooms/admin.py
+++ rooms/admin.py
@@ -17,7 +17,26 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
 
-    pass
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = ("instant_book", "city", "country")
+
+    # django-projcet -> admin pannel ModelAdmin ~
+    # icontains search ( default ) ( ^ / = / @ search operator in django admin pannel )
+    search_fields = ("=city", "^host__username")
 
 
 @admin.register(models.Photo)
