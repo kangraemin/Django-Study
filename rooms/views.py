ba8f87f176dc6636d79b836a4485962eefd81782
kangraemin <ee4787@naver.com>
2020-06-14 22:19:20 +0900

    Add render for request / response in views

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from datetime import datetime
+from django.shortcuts import render  # -> It can send httpResponse Html inside
+from django.http import HttpResponse
 
 # Create your views here.
-def all_rooms():
-    pass
+def all_rooms(request):
+    return render(request, "all_rooms")
+    # now = datetime.now()
+    # return HttpResponse(content=f"<h1>{now}</h1>")
+    # print(dir(request))
