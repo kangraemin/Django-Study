b717703637b275f4fa8f4e4b9afc1d73a7a207e4
kangraemin <ee4787@naver.com>
2020-06-15 23:52:23 +0900

    Add init template for view

--- rooms/views.py
+++ rooms/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def all_rooms(request):
-    return render(request, "all_rooms")
+    now = datetime.now()
+    hungry = True
+    return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
     # now = datetime.now()
     # return HttpResponse(content=f"<h1>{now}</h1>")
     # print(dir(request))
