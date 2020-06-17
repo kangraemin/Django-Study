ac39a4d8eb5568c7b7b4b2e5378d7371731e38dd
kangraemin <ee4787@naver.com>
2020-06-17 22:15:28 +0900

    Add pagination manually

--- rooms/views.py
+++ rooms/views.py
@@ -5,8 +5,14 @@ from . import models
 
 # Create your views here.
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
+    page = int(request.GET.get("page", 1))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    # first -> offset / second -> limit -> 모든것 가져 온다음에 20 ~ 30개를 자르는게 아니라, 장고는 lazy load 한다.
+    all_rooms = models.Room.objects.all()[offset:limit]
     return render(request, "rooms/home.html", context={"rooms": all_rooms})
+
     # now = datetime.now()
     # hungry = True
     # return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
