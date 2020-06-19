e6e68c1c4da6814656b2faf519f0cc0cf3816808
kangraemin <ee4787@naver.com>
2020-06-19 14:32:07 +0900

    Finishing the search form

--- rooms/views.py
+++ rooms/views.py
@@ -60,8 +60,10 @@ def search(request):
     beds = int(request.GET.get("beds", 0))
     bedrooms = int(request.GET.get("bedrooms", 0))
     baths = int(request.GET.get("baths", 0))
-    s_amenities = request.GET.get("amenities")
-    s_facilities = request.GET.get("facilities")
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
+    instant = request.GET.getlist("instant", False)
+    superhost = request.GET.getlist("super_host", False)
     print(s_amenities, s_facilities)
 
     amenities = models.Amenity.objects.all()
@@ -71,6 +73,10 @@ def search(request):
         "city": city,
         "s_room_type": room_type,
         "s_country": country,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+        "instant": instant,
+        "super_host": superhost,
     }
 
     choice = {
