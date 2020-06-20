f0438e28ad8888df83eef76372eb41918c110833
kangraemin <ee4787@naver.com>
2020-06-20 20:48:50 +0900

    Write github login callback function detail

--- users/views.py
+++ users/views.py
@@ -137,12 +137,45 @@ def github_login(request):
 def github_callback(request):
     client_id = os.environ.get("GH_ID")
     client_secret = os.environ.get("GH_SECRET")
-    code = request.GET.get("code", None)
+    code = request.GET.get(
+        "code", None
+    )  # code -> If you use code or receive code long time ( about 10 min ) ago, code will be useless code
     if code is not None:
-        request = requests.post(
+        result = requests.post(
             f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
             headers={"Accept": "application/json"},
         )
-        print(request.json())
+        result_json = result.json()
+        error = result_json.get("error", None)
+        if error is not None:
+            return redirect(reverse("users:login"))
+        else:
+            access_token = result_json.get("access_token")
+            api_request = requests.get(
+                f"https://api.github.com/user",
+                headers={
+                    "Authorization": f"token {access_token}",
+                    "Accept": "application/json",
+                },
+            )
+            # api_request.status_code ( it can use to check response is OK but some api send OK code although they have error in response )
+            profile_json = api_request.json()
+            username = profile_json.get("lgoin", None)
+            if username is not None:
+                name = profile_json.get("name")
+                email = profile_json.get("email")
+                bio = profile_json.get("bio")
+                user = models.User.objects.get(email=email)
+                if user is not None:
+                    return redirect(reverse("users:login"))
+                else:
+                    user = models.User.objects.create(
+                        username=email, first_name=name, bio=bio, email=email
+                    )
+                    login(request, user)
+                    return redirect(reverse("core:home"))
+            else:
+                return redirect(reverse("users:login"))
+
     else:
         return redirect(reverse("core:home"))
