d7b122ae723d1de73578ab160ca5624dd3d61224
kangraemin <ee4787@naver.com>
2020-06-21 17:28:57 +0900

    Add tailwind css / gulp ... etc

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require("gulp");
+
+const css = () => {
+  const postCss = require("gulp-postcss");
+  const sass = require("gulp-sass");
+  const minify = require("gulp-csso");
+  sass.compiler = require("node-sass");
+  return gulp
+    .src("assets/scss/styles.scss") // scss source file 
+    .pipe(sass().on("error", sass.logError)) // when error case
+    .pipe(postCss([ // It understand postcss ( tailwindcss / autoprefixer )
+      require("tailwindcss"),
+      require("autoprefixer")
+    ]))
+    .pipe(minify()) // And minify it 
+    .pipe(gulp.dest("static/css")); // and store at static/css 
+};
+
+exports.default = css;
