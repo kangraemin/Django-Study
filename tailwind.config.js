58860001fa4405d8ed4ac2e43b840bd709a16456
kangraemin <ee4787@naver.com>
2020-06-21 20:20:19 +0900

    Add tailwind extent variables in config / made main image in home

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      // spacing -> for height 
+      spacing: {
+        // for classname : css
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "75vh": "75vh"
+      },
+      borderRadius: {
+        xl: "1.5rem"
+      }
+    },
   },
   variants: {},
   plugins: [],
