8f9742fe879bb686be45aafa943a6ec32c4c0c16
kangraemin <ee4787@naver.com>
2020-06-21 17:41:58 +0900

    Add remark to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,14 @@ const css = () => {
   const minify = require("gulp-csso");
   sass.compiler = require("node-sass");
   return gulp
-    .src("assets/scss/styles.scss") // scss source file 
-    .pipe(sass().on("error", sass.logError)) // when error case
-    .pipe(postCss([ // It understand postcss ( tailwindcss / autoprefixer )
+    .src("assets/scss/styles.scss") // scss source file
+    .pipe(sass().on("error", sass.logError)) // when error case and change scss file to css file 
+    .pipe(postCss([ // It understand postcss ( tailwindcss / autoprefixer ) it transform tailwind rule like @tailwind ~ somthing to css ( browser only understand normal css )
       require("tailwindcss"),
       require("autoprefixer")
     ]))
     .pipe(minify()) // And minify it 
-    .pipe(gulp.dest("static/css")); // and store at static/css 
+    .pipe(gulp.dest("static/css")); // and store result at static/css 
 };
 
 exports.default = css;
