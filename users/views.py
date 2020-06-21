46a609ca8afa5334a5e55dd1f8744cacb78cc414
kangraemin <ee4787@naver.com>
2020-06-21 13:43:48 +0900

    Get profile from kakao account & save profile image

--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
+from django.core.files.base import ContentFile
 from . import forms, models
 
 
@@ -250,6 +251,15 @@ def kakao_callback(request):
             )
             user.set_unusable_password()
             user.save()
+            if profile_image is not None:
+                photo_request = requests.get(profile_image)
+                # Search Django models imagefield / save of filefield
+                user.avatar.save(
+                    f"{nickname}-avatar",
+                    ContentFile(
+                        photo_request.content
+                    ),  # content -> byte ( 0 / 1 ) / ContentFile(bullshit file ( byte file, 0 / 1 ))
+                )  # Don't need to save ( because filefields save )
         login(request, user)
         return redirect(reverse("core:home"))
     except KakaoException:
