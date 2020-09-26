fc42485ada862b912a2679eabe2dd6633cbb6914
kangraemin <remin1994@gmail.com>
2020-09-26 23:27:42 +0900

    Fix static file url in settings.py & Add command collectstatic

--- config/settings.py
+++ config/settings.py
@@ -167,11 +167,14 @@ EMAIL_FROM = "haha-guy@sandbox8388fef8c2534cbf95eb3c7bc37e24a0.mailgun.org"  # I
 # Sentry
 
 if not DEBUG:
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'config.custom_storages.UploadStorage'
+    STATICFILES_STORAGE = 'config.custom_storages.StaticStorage'
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = "airbnb-clone-rams"
+
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static"
 
     sentry_sdk.init(
     dsn=os.environ.get("SENTRY_URL"),
