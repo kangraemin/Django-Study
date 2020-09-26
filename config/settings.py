3497452a3ae726eb3aadebb032e64d29e876c333
kangraemin <remin1994@gmail.com>
2020-09-27 02:04:47 +0900

    Add default acl

--- config/settings.py
+++ config/settings.py
@@ -172,6 +172,7 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = "airbnb-clone-rams"
+    AWS_DEFAULT_ACL = True
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
