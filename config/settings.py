0c6e181e1a3b40a2e078d62b4c2c21e1f9bd6b8a
kangraemin <remin1994@gmail.com>
2020-09-27 22:22:00 +0900

    Remove bucket acl ( Not used )

--- config/settings.py
+++ config/settings.py
@@ -172,7 +172,6 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = "kang-test-rams"
-    AWS_BUCKET_ACL = "public-read"
     AWS_DEFAULT_ACL = "public-read"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 
