4e52d688fa673e35d3d16bb7ecfa4e8a6e331d10
kangraemin <remin1994@gmail.com>
2020-09-27 22:20:00 +0900

    Add default acl for testing

--- config/settings.py
+++ config/settings.py
@@ -173,6 +173,7 @@ if not DEBUG:
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = "kang-test-rams"
     AWS_BUCKET_ACL = "public-read"
+    AWS_DEFAULT_ACL = "public-read"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
